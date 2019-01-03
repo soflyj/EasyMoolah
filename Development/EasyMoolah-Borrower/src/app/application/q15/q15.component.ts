e5d5efc427a7c3d661f00156f57d2d4ea94b4ba4
soflyj <soflyj@gmail/.com>
2019-01-03 13:03:04 +0200

    ID Number complete

--- Development/EasyMoolah-Borrower/src/app/application/q15/q15.component.ts
+++ Development/EasyMoolah-Borrower/src/app/application/q15/q15.component.ts
@@ -1,23 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { routerTransition } from '../../common/router.animations';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { BorrowerService } from 'src/app/service/borrower.service';
+import { BorrowerApplicationLog } from 'src/app/model/borrowerapplicationLog.model';
+
 @Component({
-selector: 'app-q15',
-templateUrl: './q15.component.html',
-styleUrls: ['./q15.component.css'],
-animations: [ routerTransition ]
+    selector: 'app-q15',
+    templateUrl: './q15.component.html',
+    styleUrls: ['./q15.component.css'],
+    animations: [routerTransition]
 })
 export class Q15Component implements OnInit {
 
-constructor(private router: Router) { }
+    Q15: FormGroup;
+    URL = false;
+    StartTime: Date;
 
-ngOnInit() {
+    first_name: string;
+    last_name: string;
+    email: string;
+    mobiletel: string;
+    landtel: string;
+
+    constructor(public zone: NgZone,
+        private router: Router,
+        private route: ActivatedRoute,
+        private borrowerService: BorrowerService) { }
+
+    ngOnInit() {
+        this.StartTime = new Date();
+
+        this.Q15 = new FormGroup({
+            'first_name': new FormControl('', Validators.required),
+            'last_name': new FormControl('', Validators.required),
+            'email': new FormControl('', [Validators.required, Validators.email]),
+            'mobiletel': new FormControl('', [Validators.required, Validators.),
+            'landtel': new FormControl('', Validators.required)
+        });
+    }
+
+    Next() {
+        // tslint:disable-next-line:max-line-length
+        this.borrowerService.addBorrowerApplicationLog(new BorrowerApplicationLog('Question', 'Address?', this.Q15.value, this.StartTime.toString(), (new Date).toString()));
+
+       // this.router.navigateByUrl('/q16', { skipLocationChange: true });
+    }
+
+    Back() {
+        this.router.navigateByUrl('/bq14', { skipLocationChange: true });
+    }
 }
-
-next() {
-this.router.navigateByUrl('/q16', { skipLocationChange: true });
-}
-
-back() {
-this.router.navigateByUrl('/bq14', { skipLocationChange: true });
-}}
--- Development/EasyMoolah-Borrower/src/app/application/q16/q16.component.ts
+++ Development/EasyMoolah-Borrower/src/app/application/q16/q16.component.ts
@@ -1,23 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { routerTransition } from '../../common/router.animations';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { BorrowerService } from 'src/app/service/borrower.service';
+import { BorrowerApplicationLog } from 'src/app/model/borrowerapplicationLog.model';
+
 @Component({
-selector: 'app-q16',
-templateUrl: './q16.component.html',
-styleUrls: ['./q16.component.css'],
-animations: [ routerTransition ]
+    selector: 'app-q16',
+    templateUrl: './q16.component.html',
+    styleUrls: ['./q16.component.css'],
+    animations: [routerTransition]
 })
 export class Q16Component implements OnInit {
 
-constructor(private router: Router) { }
+    Q16: FormGroup;
+    URL = false;
+    StartTime: Date;
+    saIdParser: any;
+    require: any;
+    idnumber: string;
+    isIdNumberValid: boolean;
 
-ngOnInit() {
+    constructor(public zone: NgZone,
+        private router: Router,
+        private route: ActivatedRoute,
+        private borrowerService: BorrowerService) { }
+
+    ngOnInit() {
+        this.StartTime = new Date();
+        this.idnumber = '';
+        this.Q16 = new FormGroup({
+            'idnumber': new FormControl('', [Validators.required, this.CheckSAIdNumber.bind(this)])
+        });
+
+    }
+
+    CheckSAIdNumber(control: FormControl): { [s: string]: boolean } {
+        this.saIdParser = require('south-african-id-parser');
+        this.isIdNumberValid = this.saIdParser.validate(control.value);
+        console.log(control.value);
+        if (!this.isIdNumberValid) {
+            return { 'IdNumberValid': true };
+        } else {
+        return null;
+        }
+    }
+
+    Next() {
+        // tslint:disable-next-line:max-line-length
+        this.borrowerService.addBorrowerApplicationLog(new BorrowerApplicationLog('Question', 'ID Number', this.Q16.value, this.StartTime.toString(), (new Date).toString()));
+
+        // this.router.navigateByUrl('/q16', { skipLocationChange: true });
+    }
+
+    Back() {
+        this.router.navigateByUrl('/bq15', { skipLocationChange: true });
+    }
 }
-
-next() {
-this.router.navigateByUrl('/q17', { skipLocationChange: true });
-}
-
-back() {
-this.router.navigateByUrl('/q15', { skipLocationChange: true });
-}}
