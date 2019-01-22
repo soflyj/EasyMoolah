e317c757fcfd98291a1c45fa449cd5d557397a19
kemoso5 <32235983+kemoso5@users.noreply.github.com>
2019-01-22 21:54:47 +0200

    Testing if Lebo can commit
    Testing if Lebo can commit

--- Development/EasyMoolah-Borrower/src/app/application/q16/q16.component.ts
+++ Development/EasyMoolah-Borrower/src/app/application/q16/q16.component.ts
@@ -85,5 +85,6 @@ export class Q16Component implements OnInit {
     resolved(captchaResponse: string) {
         this.reCAPTCHA = true;
         console.log(`Resolved captcha with response ${captchaResponse}:`);
+        // Lebo Testing 22012019
     }
 }
