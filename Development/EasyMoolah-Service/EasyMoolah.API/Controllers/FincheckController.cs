e881c1f698ee78fe0f5128690a1e7c72cdcf6bcb
soflyj <soflyj@gmail.com>
2019-01-28 21:46:12 +0200

    API Fincheck working

--- Development/EasyMoolah-Service/EasyMoolah.API/Controllers/FincheckController.cs
+++ Development/EasyMoolah-Service/EasyMoolah.API/Controllers/FincheckController.cs
@@ -6,34 +6,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EasyMoolah.API.Controllers
 {
+    [Route("api/[controller]")]
     public class FincheckController : Controller
     {
-        //[HttpGet("{id}")]
-        //public async Task<IActionResult> SendEmail([FromBody]FSPResultViewModel _request)
-        //{
-        //    FSPResultViewModel FRVM = new FSPResultViewModel();
-        //    EasyMoolah.Model.Notification.Request R = new Request() { FromAddress = "jar.ninja.nas@gmail.com", Subject = "test subject", Template = "FSPResults", ToAddress = "soflyj@gmail.com", ToAddressName = "soflyj", ToAddressTitle = "Mr" };
-        //    EasyMoolah.Model.Notification.FSPResult F = new FSPResult()
-        //        { Date = "2018-01-01", Name = "Jarrod", IsSuccessful = true };
-        //    FRVM.Request = R;
-        //    FRVM.FspResult = F;
-
-        //    var temp = Newtonsoft.Json.JsonConvert.SerializeObject(FRVM);
-        //    //FSPResultViewModel _request = null;
-        //    EasyMoolah.Notification.Email email = new Email();
-        //    EasyMoolah.Model.Response response = new Response();
-
-        //    try
-        //    {
-        //        response = await email.SendFSPResults(_request);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        response.resultCode = 1;
-        //        response.Result = (ex.InnerException == null ? "" : ex.InnerException.ToString());
-        //        response.Friendly = "Failed to send results email.";
-        //    }
-        //    return Ok(response);
-        //}
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Fincheck.Integration.Intent intent = new Fincheck.Integration.Intent();
+            var response = intent.GetIntent();
+            return Ok(response);
+        }
     }
 }
