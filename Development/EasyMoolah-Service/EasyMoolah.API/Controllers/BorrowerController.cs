5c6adf8c42947d2c0e5e6af502a1961134f8797e
jramsaroop <jramsaroop@randmutual.co.za>
2019-03-26 14:20:41 +0200

    async

--- Development/EasyMoolah-Service/EasyMoolah.API/Controllers/BorrowerController.cs
+++ Development/EasyMoolah-Service/EasyMoolah.API/Controllers/BorrowerController.cs
@@ -1,5 +1,6 @@
 ﻿using EasyMoolah.Repository;
-
+using EasyMoolah.Model;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace EasyMoolah.API.Controllers
@@ -7,17 +8,17 @@ namespace EasyMoolah.API.Controllers
     public class BorrowerController : ApiController
     {
         [Route("api/borrower/insert")]
-        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        //[System.Web.Http.AcceptVerbs("GET", "POST")]
         [System.Web.Http.HttpPost]
-        public IHttpActionResult InsertBorrower([FromBody] Borrower _borrower)
+        public async Task<IHttpActionResult> InsertBorrower([FromBody] Borrower _borrower)
         {
             EasyMoolah.Domain.Borrower borrower = new Domain.Borrower();
-            var response = borrower.Insert(_borrower);
+            var response = await borrower.Insert(_borrower);
             return Ok(response);
         }
 
         [Route("api/borrower/get")]
-        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        //[System.Web.Http.AcceptVerbs("GET", "POST")]
         [System.Web.Http.HttpPost]
         public IHttpActionResult GetBorrower([FromBody] Borrower _borrower)
         {
