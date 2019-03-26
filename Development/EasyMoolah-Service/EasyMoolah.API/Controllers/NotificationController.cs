3720437228d4e1aeb007bc15502c551517fcadcf
jramsaroop <jramsaroop@randmutual.co.za>
2019-03-26 14:39:42 +0200

    Notification

---
+++ Development/EasyMoolah-Service/EasyMoolah.API/Controllers/NotificationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using EasyMoolah.Model.Notification;
+using EasyMoolah.Notification;
+
+
+namespace EasyMoolah.API.Controllers
+{
+    public class NotificationController : ApiController
+    {
+        [System.Web.Http.Route("api/notification/insert")]
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult SendNotificationConfirmationOfApplication([FromBody] Request _request)
+        {
+            Email email = new Email();
+            var response = email.SendNotificationConfirmationOfApplication(_request);
+            return Ok(response);
+        }
+    }
+}
