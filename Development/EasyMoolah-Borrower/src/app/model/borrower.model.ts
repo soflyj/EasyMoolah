fb9864f8ac6a9155a38818e816250e6ba6a96713
soflyj <soflyj@gmail.com>
2019-03-30 16:16:54 +0200

    missing
    missing

---
+++ Development/EasyMoolah-Borrower/src/app/model/borrower.model.ts
@@ -0,0 +1,211 @@
+// missing
+export class Borrower {
+
+    private _Key: string;
+    public get Key(): string {
+        return this._Key;
+    }
+    public set Key(v: string) {
+        this._Key = v;
+    }
+
+    private _ApplicationKey: string;
+    public get ApplicationKey(): string {
+        return this._ApplicationKey;
+    }
+    public set ApplicationKey(v: string) {
+        this._ApplicationKey = v;
+    }
+
+    private _IdNumber: string;
+    public get IdNumber(): string {
+        return this._IdNumber;
+    }
+    public set IdNumber(v: string) {
+        this._IdNumber = v;
+    }
+
+    private _FirstName: string;
+    public get FirstName(): string {
+        return this._FirstName;
+    }
+    public set FirstName(v: string) {
+        this._FirstName = v;
+    }
+
+    private _LastName: string;
+    public get LastName(): string {
+        return this._LastName;
+    }
+    public set LastName(v: string) {
+        this._LastName = v;
+    }
+
+    private _Email: string;
+    public get Email(): string {
+        return this._Email;
+    }
+    public set Email(v: string) {
+        this._Email = v;
+    }
+
+    private _MobileNumber: string;
+    public get MobileNumber(): string {
+        return this._MobileNumber;
+    }
+    public set MobileNumber(v: string) {
+        this._MobileNumber = v;
+    }
+
+    private _LandlineNumber: string;
+    public get LandlineNumber(): string {
+        return this._LandlineNumber;
+    }
+    public set LandlineNumber(v: string) {
+        this._LandlineNumber = v;
+    }
+
+    private _RequiredLoanAmount: string;
+    public get RequiredLoanAmount(): string {
+        return this._RequiredLoanAmount;
+    }
+    public set RequiredLoanAmount(v: string) {
+        this._RequiredLoanAmount = v;
+    }
+
+    private _RepaymentPeriod: string;
+    public get RepaymentPeriod(): string {
+        return this._RepaymentPeriod;
+    }
+    public set RepaymentPeriod(v: string) {
+        this._RepaymentPeriod = v;
+    }
+
+    private _IsUnderDebtReview: string;
+    public get IsUnderDebtReview(): string {
+        return this._IsUnderDebtReview;
+    }
+    public set IsUnderDebtReview(v: string) {
+        this._IsUnderDebtReview = v;
+    }
+
+    private _IsDeclaredInsolvent: string;
+    public get IsDeclaredInsolvent(): string {
+        return this._IsDeclaredInsolvent;
+    }
+    public set IsDeclaredInsolvent(v: string) {
+        this._IsDeclaredInsolvent = v;
+    }
+
+    private _PermissionToDoCreditCheck: string;
+    public get PermissionToDoCreditCheck(): string {
+        return this._PermissionToDoCreditCheck;
+    }
+    public set PermissionToDoCreditCheck(v: string) {
+        this._PermissionToDoCreditCheck = v;
+    }
+
+    private _IsPopicompliant: string;
+    public get IsPopicompliant(): string {
+        return this._IsPopicompliant;
+    }
+    public set IsPopicompliant(v: string) {
+        this._IsPopicompliant = v;
+    }
+
+    private _EmploymentStatus: string;
+    public get EmploymentStatus(): string {
+        return this._EmploymentStatus;
+    }
+    public set EmploymentStatus(v: string) {
+        this._EmploymentStatus = v;
+    }
+
+    private _GrossMonthlyIncome: string;
+    public get GrossMonthlyIncome(): string {
+        return this._GrossMonthlyIncome;
+    }
+    public set GrossMonthlyIncome(v: string) {
+        this._GrossMonthlyIncome = v;
+    }
+
+    private _NettMonthlyIncome: string;
+    public get NettMonthlyIncome(): string {
+        return this._NettMonthlyIncome;
+    }
+    public set NettMonthlyIncome(v: string) {
+        this._NettMonthlyIncome = v;
+    }
+
+    private _TotalMonthlyExpenses: string;
+    public get TotalMonthlyExpenses(): string {
+        return this._TotalMonthlyExpenses;
+    }
+    public set TotalMonthlyExpenses(v: string) {
+        this._TotalMonthlyExpenses = v;
+    }
+
+    private _BankName: string;
+    public get BankName(): string {
+        return this._BankName;
+    }
+    public set BankName(v: string) {
+        this._BankName = v;
+    }
+
+    private _NumberOfDependants: string;
+    public get NumberOfDependants(): string {
+        return this._NumberOfDependants;
+    }
+    public set NumberOfDependants(v: string) {
+        this._NumberOfDependants = v;
+    }
+
+    private _ResidenceType: string;
+    public get ResidenceType(): string {
+        return this._ResidenceType;
+    }
+    public set ResidenceType(v: string) {
+        this._ResidenceType = v;
+    }
+
+    private _FullAddress: string;
+    public get FullAddress(): string {
+        return this._FullAddress;
+    }
+    public set FullAddress(v: string) {
+        this._FullAddress = v;
+    }
+
+    private _StreetName: string;
+    public get StreetName(): string {
+        return this._StreetName;
+    }
+    public set StreetName(v: string) {
+        this._StreetName = v;
+    }
+
+    private _SuburbName: string;
+    public get SuburbName(): string {
+        return this._SuburbName;
+    }
+    public set SuburbName(v: string) {
+        this._SuburbName = v;
+    }
+
+    private _CityName: string;
+    public get CityName(): string {
+        return this._CityName;
+    }
+    public set CityName(v: string) {
+        this._CityName = v;
+    }
+
+    private _PostCode: string;
+    public get PostCode(): string {
+        return this._PostCode;
+    }
+    public set PostCode(v: string) {
+        this._PostCode = v;
+    }
+}
---
+++ Development/EasyMoolah-Borrower/src/app/model/fincheck.model.ts
@@ -0,0 +1,227 @@
+export class Fincheck {
+
+    private _first_name: string;
+    public get first_name(): string {
+        return this._first_name;
+    }
+    public set first_name(v: string) {
+        this._first_name = v;
+    }
+
+    private _last_name: string;
+    public get last_name(): string {
+        return this._last_name;
+    }
+    public set last_name(v: string) {
+        this._last_name = v;
+    }
+
+    private _cell_phone_number: string;
+    public get cell_phone_number(): string {
+        return this._cell_phone_number;
+    }
+    public set cell_phone_number(v: string) {
+        this._cell_phone_number = v;
+    }
+
+    private _email: string;
+    public get email(): string {
+        return this._email;
+    }
+    public set email(v: string) {
+        this._email = v;
+    }
+
+    private _id_number: string;
+    public get id_number(): string {
+        return this._id_number;
+    }
+    public set id_number(v: string) {
+        this._id_number = v;
+    }
+
+    private _intent_id: string;
+    public get intent_id(): string {
+        return this._intent_id;
+    }
+    public set intent_id(v: string) {
+        this._intent_id = v;
+    }
+
+    private _gross_income: string;
+    public get gross_income(): string {
+        return this._gross_income;
+    }
+    public set gross_income(v: string) {
+        this._gross_income = v;
+    }
+
+    private _net_income: string;
+    public get net_income(): string {
+        return this._net_income;
+    }
+    public set net_income(v: string) {
+        this._net_income = v;
+    }
+
+    private _citizen: string;
+    public get citizen(): string {
+        return this._citizen;
+    }
+    public set citizen(v: string) {
+        this._citizen = v;
+    }
+
+    private _debt_review: string;
+    public get debt_review(): string {
+        return this._debt_review;
+    }
+    public set debt_review(v: string) {
+        this._debt_review = v;
+    }
+
+    private _popi: string;
+    public get popi(): string {
+        return this._popi;
+    }
+    public set popi(v: string) {
+        this._popi = v;
+    }
+
+    private _debt_review_opt: string;
+    public get debt_review_opt(): string {
+        return this._debt_review_opt;
+    }
+    public set debt_review_opt(v: string) {
+        this._debt_review_opt = v;
+    }
+
+    private _repayment_period: string;
+    public get repayment_period(): string {
+        return this._repayment_period;
+    }
+    public set repayment_period(v: string) {
+        this._repayment_period = v;
+    }
+
+    private _loan_amount_required: string;
+    public get loan_amount_required(): string {
+        return this._loan_amount_required;
+    }
+    public set loan_amount_required(v: string) {
+        this._loan_amount_required = v;
+    }
+
+    private _expenses: string;
+    public get expenses(): string {
+        return this._expenses;
+    }
+    public set expenses(v: string) {
+        this._expenses = v;
+    }
+
+    private _education: string;
+    public get education(): string {
+        return this._education;
+    }
+    public set education(v: string) {
+        this._education = v;
+    }
+
+    private _street_address: string;
+    public get street_address(): string {
+        return this._street_address;
+    }
+    public set street_address(v: string) {
+        this._street_address = v;
+    }
+
+    private _suburb: string;
+    public get suburb(): string {
+        return this._suburb;
+    }
+    public set suburb(v: string) {
+        this._suburb = v;
+    }
+    
+    private _city : string;
+    public get city() : string {
+        return this._city;
+    }
+    public set city(v : string) {
+        this._city = v;
+    }
+    
+    private _payday : string;
+    public get payday() : string {
+        return this._payday;
+    }
+    public set payday(v : string) {
+        this._payday = v;
+    }
+    
+    private _province : string;
+    public get province() : string {
+        return this._province;
+    }
+    public set province(v : string) {
+        this._province = v;
+    }
+    
+    private _post_code : string;
+    public get post_code() : string {
+        return this._post_code;
+    }
+    public set post_code(v : string) {
+        this._post_code = v;
+    }
+    
+    private _employed : string;
+    public get employed() : string {
+        return this._employed;
+    }
+    public set employed(v : string) {
+        this._employed = v;
+    }
+    
+    private _employment_period : string;
+    public get employment_period() : string {
+        return this._employment_period;
+    }
+    public set employment_period(v : string) {
+        this._employment_period = v;
+    }
+    
+    private _work_phone_string : string;
+    public get work_phone_string() : string {
+        return this._work_phone_string;
+    }
+    public set work_phone_string(v : string) {
+        this._work_phone_string = v;
+    }
+    
+    private _payment_frequency : string;
+    public get payment_frequency() : string {
+        return this._payment_frequency;
+    }
+    public set payment_frequency(v : string) {
+        this._payment_frequency = v;
+    }
+    
+    private _bank_name : string;
+    public get bank_name() : string {
+        return this._bank_name;
+    }
+    public set bank_name(v : string) {
+        this._bank_name = v;
+    }
+    
+    private _referee_id : string;
+    public get referee_id() : string {
+        return this._referee_id;
+    }
+    public set referee_id(v : string) {
+        this._referee_id = v;
+    }
+    
+}
