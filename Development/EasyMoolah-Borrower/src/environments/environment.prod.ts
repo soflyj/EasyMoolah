c50af0aeea345ff645cdc50a799a0457f4beba87
Jarrod <jarrod.ramsaroop@hotmail.com>
2019-05-26 14:49:47 +0200

    Created of master

--- Development/EasyMoolah-Borrower/src/environments/environment.prod.ts
+++ Development/EasyMoolah-Borrower/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://service.easymoolah.co.za/api/',
   localToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImVhc3ltb29sYWgiLCJuYmYiOjE1NTM2MTExNjksImV4cCI6MTU1NDIxNTk2OSwiaWF0IjoxNTUzNjExMTY5LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMTkxIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MDE5MSJ9.Ow461m1MkOn9SCd-7qWfFCkfnJfp4GGLGtIh4HTCmvQ',
-  debug: false
+  debug: true
 };
