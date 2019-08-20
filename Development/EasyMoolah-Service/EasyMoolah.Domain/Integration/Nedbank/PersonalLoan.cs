6d7e6da2f2b0e421baf0ca016c71eb35fcd344c5
soflyj <soflyj@gmail.com>
2019-08-21 00:13:47 +0200

    complete 9 and need to test

---
+++ Development/EasyMoolah-Service/EasyMoolah.Domain/Integration/Nedbank/PersonalLoan.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using EasyMoolah.Model;
+using EasyMoolah.Repository;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Nedbank.Integration;
+
+namespace EasyMoolah.Domain.Integration
+{
+    public class PersonalLoan
+    {
+        EasyMoolah.Domain.Logs logs = new Logs();
+        Nedbank.Integration.PersonalLoan personalLoan = new Nedbank.Integration.PersonalLoan();
+
+        /// <summary>
+        /// Step 9
+        /// https://api.nedbank.co.za/apimarket/sandbox/open-banking/v1/personal-loan
+        /// </summary>
+        /// <param name="intentRequest"></param>
+        /// <param name="lightToken"></param>
+        /// <returns></returns>
+        public async Task<Model.Nedbank.PersonLoanResponse.RootObject> AcceptPersonalLoan(Model.Nedbank.PersonLoanRequest.RootObject personalLoanRequest, string lightToken)
+        {
+            var result = personalLoan.AcceptPersonalLoan(personalLoanRequest, lightToken);
+            await logs.LogIntegration(result);
+
+            var response = JsonConvert.DeserializeObject<Model.Nedbank.PersonLoanResponse.RootObject>(result.Output);
+
+            return response;
+        }
+    }
+}
